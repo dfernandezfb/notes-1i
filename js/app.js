ab3cd70ade2d2a1c8535e167896585854d581df9
Diego Fernández <dfernandezfb@gmail.com>
2022-06-15 21:09:57 -0300

    first commit with foundations

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//* 1-Mostrar datos guardados en LS // DIEGO
+//* 2-Tomar los datos del formulario e ir mostrandolos en notita de ejemplo // PAU
+//* 3-Guardar los datos de nueva nota en LS y mostrarlos // PAU
+//* 4-Agregar botón para eliminar // DIEGO
+
+
+//* 1-Mostrar datos guardados en LS // DIEGO
+
+
+//* 2-Tomar los datos del formulario e ir mostrandolos en notita de ejemplo // PAU
+
+
+//* 3-Guardar los datos de nueva nota en LS y mostrarlos // PAU
+
+
+//* 4-Agregar botón para eliminar // DIEGO
